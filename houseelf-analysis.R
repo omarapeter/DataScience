a5481a9c852848be977ef703303fd60b71ca361c
Phillip D. Alderman <phillip.alderman@okstate.edu>
2017-10-24 11:54:02 -0500

    Adding function to classify lengths

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -14,3 +14,8 @@ dna_sequence <- function(sequence) {
   return(gc_content)
 }
 
+get_size_class <- function(ear_length){
+   # Calculate the size class for one or more earth lengths
+   ear_lengths <- ifelse(ear_length > 10, "large", "small")
+   return(ear_lengths)
+}

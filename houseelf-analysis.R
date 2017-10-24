bfab04b85527b71ac5ccb81e02448254dea6d345
Peter Omara <peter.omara@okstate.edu>
2017-10-24 11:44:54 -0500

    dna_sequence code has been added

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,4 +1,16 @@
 
 
+#install.packages("stringr")
+library(stringr)
+
+
 data1 <- read.csv("data/houseelf_earlength_dna_data_1.csv")
+
+dna_sequence <- function(sequence) {
+  sequence <- str_to_lower(sequence)
+  Gs <- str_count(sequence, "g")
+  Cs <- str_count(sequence, "c")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100 
+  return(gc_content)
+}
 
